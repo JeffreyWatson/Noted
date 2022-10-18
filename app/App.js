2223862894fe362497c3c2e8f3e6b35800e62f81
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-10-18 13:43:04 -0600

    hereTyler

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import { NotesController } from "./Controllers/NotesController.js";
 import { ValuesController } from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
+  // valuesController = new ValuesController();
   notesController = new NotesController();
 
 }
