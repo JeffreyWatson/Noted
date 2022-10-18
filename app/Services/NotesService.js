63124a4b1c32dff2f6bb67dea72f98a4911936c2
TylerRice27 <Tylerrayrice2@gmail.com>
2022-10-18 17:27:20 -0600

    Delete notes in the service

--- app/Services/NotesService.js
+++ app/Services/NotesService.js
@@ -4,12 +4,20 @@ import { saveState } from "../Utils/Store.js"
 
 class NotesService {
 
-  createNote(data) {
-    let note = new Note(data)
-    appState.notes = [...appState.notes, note]
-    console.log(appState.notes)
-    saveState('notes', appState.notes)
-  }
+    createNote(data) {
+        let note = new Note(data)
+        appState.notes = [...appState.notes, note]
+        console.log(appState.notes)
+        saveState('notes', appState.notes)
+    }
+
+    deleteNote(id) {
+        appState.notes = appState.notes.filter(n => n.id != id)
+
+        saveState('notes', appState.notes)
+
+
+    }
 
 }
 
