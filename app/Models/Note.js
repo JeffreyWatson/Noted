dd8f3d43b8be652f7a87dad73b1bc8778758b4b5
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-10-18 19:58:09 -0600

    ph

--- app/Models/Note.js
+++ app/Models/Note.js
@@ -17,7 +17,7 @@ export class Note {
 
   get Template1() {
     return `
-        <textarea class="vh-100" cols="99" onblur="app.notesController.editBody('${this.id}')" type="text" name="body" id="body" value="text" style="height: 50px">${this.body}</textarea>
+        <textarea class="vh-100" cols="99" placeholder="Start taking notes..." onblur="app.notesController.editBody('${this.id}')" type="text" name="body" id="body" value="text" style="height: 50px">${this.body}</textarea>
     `
   }
 
