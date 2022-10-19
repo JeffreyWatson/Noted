72c478cf4bea21fc75e9a211f83e4e9713c290fa
TylerRice27 <Tylerrayrice2@gmail.com>
2022-10-18 20:10:02 -0600

    pop confirm changes

--- app/Controllers/NotesController.js
+++ app/Controllers/NotesController.js
@@ -5,57 +5,57 @@ import { Pop } from "../Utils/Pop.js"
 
 
 function _drawNotes() {
-  let notes = appState.notes
-  let template = ''
-  notes.forEach(n => template += n.Template)
-  // @ts-ignore
-  document.getElementById('notes').innerHTML = template
+    let notes = appState.notes
+    let template = ''
+    notes.forEach(n => template += n.Template)
+    // @ts-ignore
+    document.getElementById('notes').innerHTML = template
 }
 
 function _drawBody(id) {
-  let notes = appState.notes
-  let template = ''
-  notes.find(n => template += n.Template1)
-  // @ts-ignore
-  document.getElementById('body').innerHTML = template
+    let notes = appState.notes
+    let template = ''
+    notes.find(n => template += n.Template1)
+    // @ts-ignore
+    document.getElementById('body').innerHTML = template
 }
 
 
 export class NotesController {
 
-  constructor() {
-    appState.on('notes', _drawNotes)
-    _drawNotes()
+    constructor() {
+        appState.on('notes', _drawNotes)
+        _drawNotes()
 
-  }
-  createNote() {
-    try {
-      window.event?.preventDefault()
-      let form = window.event?.target
-      let data = getFormData(form)
-      console.log("notes getting created in my controller", data)
-      notesService.createNote(data)
-      // @ts-ignore
-      form.reset()
-    } catch (error) {
-      console.error("creating a note", error)
     }
-  }
-
-  editBody(id) {
-    _drawBody(id)
-    let textarea = window.event?.target
-    console.log('drawing body', textarea);
-    // @ts-ignore
-    notesService.editBody(textarea.value, id)
-  }
-
-  async deleteNote(id) {
-    // @ts-ignore
-    if (await confirm('Delete note?', 'Are you sure', 'warning')) {
-      notesService.deleteNote(id)
-      Pop.toast('Deleting Note', 'success')
+    createNote() {
+        try {
+            window.event?.preventDefault()
+            let form = window.event?.target
+            let data = getFormData(form)
+            console.log("notes getting created in my controller", data)
+            notesService.createNote(data)
+            // @ts-ignore
+            form.reset()
+        } catch (error) {
+            console.error("creating a note", error)
+        }
     }
-  }
+
+    editBody(id) {
+        _drawBody(id)
+        let textarea = window.event?.target
+        console.log('drawing body', textarea);
+        // @ts-ignore
+        notesService.editBody(textarea.value, id)
+    }
+
+    async deleteNote(id) {
+        // @ts-ignore
+        if (await Pop.confirm('Delete note?', 'Are you sure', 'Yes')) {
+            notesService.deleteNote(id)
+            Pop.toast('Deleting Note', 'success')
+        }
+    }
 
 }
